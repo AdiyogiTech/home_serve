aa4a2d67bad52529e68750f79385752df50a3046
AdiyogiTech <adiyogiappdeveloper@gmail.com>
2026-04-09 09:40:40 +0530

    first commit

---
+++ lib/view/splashScreen/splashScreen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Color(0xFFFF6600), // Deep Orange
+              Color(0xFFFFB347), // Light Orange
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            'HomeServe Pro',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
